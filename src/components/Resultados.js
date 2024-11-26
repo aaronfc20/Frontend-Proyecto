f182d666769e109d16eff19eef677439f927c384
Fait654 <fait6543210@gmail.com>
2024-11-26 15:36:16 -0500

    test  robertp sfsfsdf

--- src/components/Resultados.js
+++ src/components/Resultados.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
 function Resultados() {
   const [tests, setTests] = useState([]);
 
+
   useEffect(() => {
-    axios.get("http://localhost:3001/api/test-de-presion/resultados")
-      .then(response => {
+    axios
+      .get("http://localhost:3001/api/test-de-presion/resultados")
+      .then((response) => {
         setTests(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Hubo un error al obtener los resultados:", error);
       });
   }, []);
 
+
   return (
     <div>
       <h1>Resultados del Test de Depresión</h1>
-      
+     
       <h2>Detalles de los Tests</h2>
       {tests.length === 0 ? (
         <p>No se han encontrado resultados.</p>
@@ -25,7 +29,7 @@ function Resultados() {
         tests.map((test, index) => (
           <div key={index}>
             <p><strong>Fecha y Hora:</strong> {new Date(test.fecha).toLocaleString()}</p>
-            <p><strong>Probabilidad de depresión:</strong> {test.probabilidad}</p>
+            <p><strong>Probabilidad de depresión:</strong> {test.probabilidadDepresion}</p>
           </div>
         ))
       )}
@@ -33,6 +37,10 @@ function Resultados() {
   );
 }
 
+
 export default Resultados;
+
+
+
 
 
